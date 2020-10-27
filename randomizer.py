15fff1aebfb23d5d35a4f8c7be723e28df94c2bf
TRottinger <TRRottinger@gmail.com>
2020-10-26 23:08:13 -0400

    Refactoring to move twitch functionality to twitch.py

--- randomizer.py
+++ randomizer.py
@@ -2,6 +2,7 @@ import requests
 import random
 import os
 from dotenv import load_dotenv
+import twitch
 from url_builder import build_url
 
 load_dotenv()
@@ -11,62 +12,16 @@ CLIENT_SECRET = os.getenv('TWITCH_CLIENT_SECRET')
 
 def get_random_twitch_stream():
 
-    headers = {
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-        'grant_type': 'client_credentials'
-    }
+    games = twitch.get_twitch_games()
 
-    response = requests.post('https://id.twitch.tv/oauth2/token', data=headers)
+    game_id_picked = random.choice(list(games))
+    game_name_picked = str(games.get(game_id_picked))
 
-    access_token = response.json()['access_token']
-
-    streams_request_url = 'https://api.twitch.tv/helix/streams'
-
-    headers = {
-        'client-id': CLIENT_ID,
-        'Authorization': 'Bearer ' + access_token,
-    }
-
-    response = requests.get('https://api.twitch.tv/helix/games/top?first=100', headers=headers)
-    games = response.json()['data']
-
-    game_ids = []
-    game_dict = {}
-
-    for game in games:
-        game_ids.append(game['id'])
-        game_dict[game['id']] = game['name']
-
-    game_id_picked = random.choice(list(game_dict))
-    game_name_picked = str(game_dict.get(game_id_picked))
-
-    after = '0'
-    streamers = []
-
-    for i in range(0, 10):
-
-        if after != '0':
-            request_url = build_url(streams_request_url, "first=100", "game_id=" + game_id_picked, "after=" + after)
-        else:
-            request_url = build_url(streams_request_url, "first=100", "game_id=" + game_id_picked)
-
-        response = requests.get(request_url, headers=headers)
-
-        for data in response.json()['data']:
-            streamers.append(data)
-
-        if str(response.json()['pagination']) == '{}':
-            break
-        else:
-            pag = response.json()['pagination']['cursor']
-            after = pag
+    streamers = twitch.get_twitch_streams(game_id_picked)
 
     streamer = random.choice(streamers)
 
-    response = requests.get('https://api.twitch.tv/helix/users?id=' + streamer['user_id'], headers=headers)
-    data = response.json()['data']
-    streamer_login_name = (data[0]['login'])
+    streamer_login_name = twitch.get_streamer_login_name(streamer)
 
     print('Returning: ' + str(streamer_login_name), str(streamer['viewer_count']), game_name_picked)
     return str(streamer_login_name), str(streamer['viewer_count']), game_name_picked
---
+++ twitch.py
@@ -0,0 +1,103 @@
+import requests
+import random
+import os
+from dotenv import load_dotenv
+from url_builder import build_url
+
+load_dotenv()
+CLIENT_ID = os.getenv('TWITCH_CLIENT_ID')
+CLIENT_SECRET = os.getenv('TWITCH_CLIENT_SECRET')
+
+
+def get_twitch_access_token():
+    headers = {
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'grant_type': 'client_credentials'
+    }
+
+    response = requests.post('https://id.twitch.tv/oauth2/token', data=headers)
+
+    access_token = response.json()['access_token']
+
+    return access_token
+
+
+# Returns a dictionary of top 100 twitch games
+def get_twitch_games():
+    access_token = get_twitch_access_token()
+
+    headers = {
+        'client-id': CLIENT_ID,
+        'Authorization': 'Bearer ' + access_token,
+    }
+
+    response = requests.get('https://api.twitch.tv/helix/games/top?first=100', headers=headers)
+    games = response.json()['data']
+
+    game_dict = {}
+
+    for game in games:
+        game_dict[game['id']] = game['name']
+
+    return game_dict
+
+
+def build_streams_url(url, first, game_id, after):
+    return_url = ''
+    if game_id != '0':
+        if after != '0':
+            return_url = build_url(url, "first=" + first, "game_id=" + game_id, "after=" + after)
+        else:
+            return_url = build_url(url, "first=" + first, "game_id=" + game_id)
+    else:
+        if after != '0':
+            return_url = build_url(url, "first=" + first, "after=" + after)
+        else:
+            return_url = build_url(url, "first=" + first)
+
+    return return_url
+
+
+def get_twitch_streams(game_id=0):
+
+    streams_request_url = 'https://api.twitch.tv/helix/streams'
+
+    access_token = get_twitch_access_token()
+
+    headers = {
+        'client-id': CLIENT_ID,
+        'Authorization': 'Bearer ' + access_token,
+    }
+
+    after = '0'
+    streamers = []
+
+    for i in range(0, 10):
+        request_url = build_streams_url(streams_request_url, '100', str(game_id), after)
+
+        response = requests.get(request_url, headers=headers)
+
+        for data in response.json()['data']:
+            streamers.append(data)
+
+        if str(response.json()['pagination']) == '{}':
+            break
+        else:
+            pag = response.json()['pagination']['cursor']
+            after = pag
+
+    return streamers
+
+
+def get_streamer_login_name(streamer):
+    access_token = get_twitch_access_token()
+
+    headers = {
+        'client-id': CLIENT_ID,
+        'Authorization': 'Bearer ' + access_token,
+    }
+    response = requests.get('https://api.twitch.tv/helix/users?id=' + streamer['user_id'], headers=headers)
+    data = response.json()['data']
+    streamer_login_name = (data[0]['login'])
+    return streamer_login_name
