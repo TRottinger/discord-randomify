849b7d7192a4452ef7b4dda4274420b5d18ac312
TRottinger <TRRottinger@gmail.com>
2020-10-28 04:41:42 -0400

    Refactoring this whole thing to not be literal garbage that doesn't use any libraries

--- bot.py
+++ bot.py
@@ -2,11 +2,20 @@ import os
 
 from discord.ext import commands
 from dotenv import load_dotenv
+import logging
+
+logging.basicConfig(filename='info.log', filemode='w', level=logging.INFO)
+log = logging.getLogger(__name__)
+
 # Load twitch client id and secret into file
 load_dotenv()
 CLIENT_ID = os.getenv('TWITCH_CLIENT_ID')
 CLIENT_SECRET = os.getenv('TWITCH_CLIENT_SECRET')
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+log.info('CLIENT_ID: ' + CLIENT_ID)
+log.info('CLIENT_SECRET: ' + CLIENT_SECRET)
+log.info('TOKEN: ' + TOKEN)
 
 
 class Bot(commands.AutoShardedBot):
@@ -17,7 +26,6 @@ class Bot(commands.AutoShardedBot):
         self.load_extension('cogs.wiki')
         self.load_extension('cogs.common_randomizers')
         super().run(TOKEN, reconnect=True)
-        print('Starting bot')
 
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
