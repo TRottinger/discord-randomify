7efd6efa003ff23d2739a44a1fd966ef1814559a
TRottinger <TRRottinger@gmail.com>
2020-10-29 14:50:53 -0400

    Adding User Agent to header for reddit calls

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-import urllib.request
+from urllib.request import Request
 import urllib.error
 
 
@@ -7,13 +7,16 @@ class Reddit(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="subreddit", description="Get a link to a random subreddit", brief="Get a random subreddit")
+    @commands.command(name="subreddit", description="Get a link to a random subreddit", brief="Get a random subreddit",
+                      aliases=["reddit"])
     async def subreddit(self, ctx):
         try:
-            urlopen = urllib.request.urlopen('https://www.reddit.com/r/random')
-            result = str(urlopen.url)
+            urlopen = urllib.request.Request('https://www.reddit.com/r/random')
+            urlopen.add_header('User-Agent', 'discord-bot/0.0.1')
+            response = urllib.request.urlopen(urlopen)
+            result = str(response.url)
         except urllib.error.HTTPError:
-            result = 'I\'m being rate limited :('
+            result = 'I\'m being rate limited, so manually click this: https://www.reddit.com/r/random'
         author = ctx.author.mention
         await ctx.send(author + ' ' + result + '')
 
