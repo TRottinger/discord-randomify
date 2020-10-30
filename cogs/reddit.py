e9b042895e8248dd65e25d4314cef134e89caee1
TRottinger <TRRottinger@gmail.com>
2020-10-30 02:27:53 -0400

    Cleaning up code quality warnings

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -13,8 +13,9 @@ class Reddit(commands.Cog):
         try:
             urlopen = urllib.request.Request('https://www.reddit.com/r/random')
             urlopen.add_header('User-Agent', 'discord-bot/0.0.1')
-            with urllib.request.urlopen(urlopen) as response:
-                result = str(response.url)
+            if urlopen.full_url.startswith('https'):
+                with urllib.request.urlopen(urlopen) as response:
+                    result = str(response.url)
         except urllib.error.HTTPError:
             result = 'I\'m being rate limited, so manually click this: https://www.reddit.com/r/random'
         author = ctx.author.mention
