4f7dea0628de5bdc6619fe6c49697b801afffc79
TRottinger <TRRottinger@gmail.com>
2020-11-01 07:14:07 -0500

    Adding tests and cleaning up command call names

--- cogs/twitch.py
+++ cogs/twitch.py
@@ -17,18 +17,19 @@ def prepare_output_string(author, streamer_name, game_name, viewer_count):
 class Twitch(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.twitch_helpers = twitch_helpers.TwitchHelpers()
 
     @commands.command(name="twitch", description="Get a link to a random streamer", aliases=["stream", "streamer"],
                       brief="Get a random twitch streamer")
     async def twitch(self, ctx):
         log.info('Got twitch call')
-        games, weighted_id_game_selector = twitch_helpers.get_twitch_games()
+        games, weighted_id_game_selector = self.twitch_helpers.get_twitch_games()
         log.info('Got weighted_id_game_selector_size ' + str(len(weighted_id_game_selector)))
         game_id_picked = random.choice(weighted_id_game_selector)
         game_name_picked = str(games.get(game_id_picked))
         log.info('Got game: ' + game_name_picked)
 
-        streamer = twitch_helpers.get_streamer(game_id_picked)
+        streamer = self.twitch_helpers.get_streamer(game_id_picked)
         author = ctx.author.mention
         if streamer is None:
             await ctx.send(author + ' I did not find any streamers')
@@ -38,18 +39,18 @@ class Twitch(commands.Cog):
             log.info('Sending out result string: ' + result_string)
             await ctx.send(result_string)
 
-    @commands.command(name="twitch_game", description="Get a link to a random streamer playing a specific game",
-                      aliases=["game_stream", "twitchgame"], usage="<game_name>",
+    @commands.command(name="twitchgame", description="Get a link to a random streamer playing a specific game",
+                      aliases=["game_stream", "twitch_game"], usage="<game_name>",
                       brief="Get a random twitch streamer by game")
     async def twitch_game(self, ctx, *, arg):
         game_name_picked = arg
-        game_id_picked = twitch_helpers.get_game_by_name(game_name_picked)
+        game_id_picked = self.twitch_helpers.get_game_by_name(game_name_picked)
 
         author = ctx.author.mention
         if game_id_picked == '':
             await ctx.send(author + ' I did not find the game ' + game_name_picked)
             return
-        streamer = twitch_helpers.get_streamer(game_id_picked)
+        streamer = self.twitch_helpers.get_streamer(game_id_picked)
         if streamer is None:
             await ctx.send(author + ' I did not find any streamers under the game ' + game_name_picked)
         else:
---
+++ tests/http_helpers_test.py
@@ -0,0 +1,76 @@
+import unittest
+from unittest import TestCase, mock
+from utils import http_helpers
+
+
+class Response:
+    def __init__(self, status=0):
+        self.status_code = status
+
+
+# This method will be used by the mock to replace requests.get
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    if args[0] == 'http://url.com/':
+        return MockResponse({"key1": "value1"}, 200)
+    return MockResponse(None, 404)
+
+
+class HttpHelpersTests(TestCase):
+    def test_handle_status_code_no_value(self):
+        response = Response()
+        status = http_helpers.handle_status_code(response)
+        self.assertEqual(status, 'Unknown')
+
+    def test_handle_status_code_bad_request(self):
+        response = Response(400)
+        status = http_helpers.handle_status_code(response)
+        self.assertEqual(status, 'Bad Request')
+
+    def test_handle_status_code_unauthorized(self):
+        response = Response(401)
+        status = http_helpers.handle_status_code(response)
+        self.assertEqual(status, 'Unauthorized')
+
+    def test_handle_status_code_forbidden(self):
+        response = Response(403)
+        status = http_helpers.handle_status_code(response)
+        self.assertEqual(status, 'Forbidden')
+
+    def test_handle_status_code_not_found(self):
+        response = Response(404)
+        status = http_helpers.handle_status_code(response)
+        self.assertEqual(status, 'Not Found')
+
+    def test_handle_status_code_ok(self):
+        response = Response(200)
+        status = http_helpers.handle_status_code(response)
+        self.assertEqual(status, 'OK')
+
+    def test_handle_status_code_server_error(self):
+        response = Response(500)
+        status = http_helpers.handle_status_code(response)
+        self.assertEqual(status, 'Server Error')
+
+    def test_handle_status_code_service_unavailable(self):
+        response = Response(503)
+        status = http_helpers.handle_status_code(response)
+        self.assertEqual(status, 'Service Unavailable')
+
+    @mock.patch('utils.http_helpers.requests.get', side_effect=mocked_requests_get)
+    def test_get_random_query(self, mock_get):
+        resp = http_helpers.send_get_request('http://url.com/', headers=None)
+        self.assertIn(mock.call('http://url.com/', headers=None), mock_get.call_args_list)
+        self.assertEqual(resp.json(), {"key1": "value1"})
+        self.assertEqual(resp.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
