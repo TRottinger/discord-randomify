2e1480f371a43123b3ecec1a237809788b6c21a0
TRottinger <TRRottinger@gmail.com>
2020-11-03 23:00:28 -0500

    Adding more commands.

--- cogs/admin.py
+++ cogs/admin.py
@@ -8,6 +8,36 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def load_extension(self, ctx, *, extension):
+        try:
+            self.bot.load_extension(extension)
+        except commands.ExtensionError as e:
+            await ctx.send('Invalid extension')
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def reload_extension(self, ctx, *, extension):
+        try:
+            self.bot.reload_extension(extension)
+        except commands.ExtensionError as e:
+            await ctx.send('Invalid extension')
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def unload_extension(self, ctx, *, extension):
+        try:
+            self.bot.unload_extension(extension)
+        except commands.ExtensionError as e:
+            await ctx.send('Invalid extension')
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def get_guilds(self, ctx):
+        guilds = self.bot.guilds
+        await ctx.author.send([guild.name for guild in guilds])
+
 
 def setup(bot):
     bot.add_cog(Admin(bot))
